14105c430be0737066e3e659fa738f51d8ec08d0
LazyElephant <tschmidt@lazyelephantstudio.com>
2017-01-17 23:03:37 -0500

    exclued __v from api response object

--- controllers/ApiController.js
+++ controllers/ApiController.js
@@ -19,7 +19,7 @@ module.exports.Search = function(req, res, next) {
 }
 
 module.exports.Latest = function(req, res, next) {
-  searchTerm.find().select({"_id": 0}).exec( function(err, terms) {
+  searchTerm.find().select({"_id": 0, "__v": 0}).exec( function(err, terms) {
     if (err) return res.status(500).json({ "error" : "Internal Database Error" });
 
     res.status(200).json(terms);
